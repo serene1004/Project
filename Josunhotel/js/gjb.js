4ed40713c3961bdfa53a2b7be47dc422834072b9
serene1004 <vcbxnzn@naver.com>
2021-03-12 18:46:08 +0900

    update 2021-03-12

--- Josunhotel/js/gjb.js
+++ Josunhotel/js/gjb.js
@@ -3,20 +3,74 @@
 
     var gjb = {
         init:function(){
-            this.headerFn();
-            this.section1Fn();
-            this.section2Fn();
-            this.section3Fn();
-            this.section4Fn();
-            this.section5Fn();
-            this.section6Fn();
-            this.section7Fn();
-            this.indicatorFn();
-            this.footerFn();
+            var taht = this;
+
+            taht.headerFn();
+            taht.section1Fn();
+            taht.section2Fn();
+            taht.section3Fn();
+            taht.section4Fn();
+            taht.section5Fn();
+            taht.section6Fn();
+            taht.section7Fn();
+            taht.indicatorFn();
+            taht.footerFn();
         },
 
         headerFn:function(){
+            var $nav = $('#header #nav');
+            var $logo = $('#header #logo h1 a');
+            var $gnbWrap = $('#header #nav .gnb-wrap');
+            var $gnbBtn = $('#header #nav .gnb-wrap .gnb-btn');
+            var $lnbWrap = $('#header #nav .gnb-wrap .lnb-wrap');
+
+            var $sideBtn = $('#header .side-btn-box .side-btn')
+            var $sideBtnText = $('#header .side-btn-box .side-btn .side-btn-text');
+
+            $sideBtn.on({
+                mouseenter:function(){
+                    $sideBtnText.stop().slideDown(200);
+                },
+                mouseleave:function(){
+                    $sideBtnText.stop().slideUp(200);
+                }
+            });
+
+            // gnb에 마우스오버시 lnb보이게
+            // lnb영역으로 마우스들어가도 영역이 닫히지않도록
+            // gnb버튼에서 다른 gnb버튼으로 마우스 이동시 기존 lnb는 닫고 새로운 lnb가 보이게
+            // gnb에 마우스오버시 nav 배경 흰색+lnb슬라이드다운
+            // 배경이 흰색이면 글자색은 검정으로, 배경이 투명이면 글자색은 흰색으로
+
+            $nav.on({
+                mouseleave:function(){
+                    $nav.removeClass('mouseon');
+                    $lnbWrap.stop().slideUp(0);
+                    $logo.css({color:'#fff'});
+
+                }
+            });
+
+            $gnbWrap.on({
+                mouseenter:function(){
+                    $nav.addClass('mouseon');
+                    $logo.css({color:'#000'});
+                }
+            });
             
+            $gnbBtn.on({
+                mouseenter:function(){
+                    $nav.addClass('mouseon');
+                    $lnbWrap.stop().slideUp(100);
+                    $(this).next().stop().slideDown(300);
+                }
+            });
+            $gnbWrap.on({
+                mouseleave:function(){
+                    $lnbWrap.stop().slideUp(100);
+                }
+            });
+
         },
 
         section1Fn:function(){
