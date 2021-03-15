939cbbf9802b2f996a67634c81d0a8017c961c8e
serene1004 <vcbxnzn@naver.com>
2021-03-15 18:42:29 +0900

    2021-03-15

--- Josunhotel/js/gjb.js
+++ Josunhotel/js/gjb.js
@@ -24,17 +24,14 @@
             var $gnbBtn = $('#header #nav .gnb-wrap .gnb-btn');
             var $lnbWrap = $('#header #nav .gnb-wrap .lnb-wrap');
 
+            var $main = $('#main');
+
+            var $langKR = $('#header .lang-KR');
+            var $langEN = $('#header .lang-EN');
+
             var $sideBtn = $('#header .side-btn-box .side-btn')
             var $sideBtnText = $('#header .side-btn-box .side-btn .side-btn-text');
 
-            $sideBtn.on({
-                mouseenter:function(){
-                    $sideBtnText.stop().slideDown(200);
-                },
-                mouseleave:function(){
-                    $sideBtnText.stop().slideUp(200);
-                }
-            });
 
             // gnb에 마우스오버시 lnb보이게
             // lnb영역으로 마우스들어가도 영역이 닫히지않도록
@@ -47,14 +44,14 @@
                     $nav.removeClass('mouseon');
                     $lnbWrap.stop().slideUp(0);
                     $logo.css({color:'#fff'});
-
+                    $main.css({filter:'brightness(100%)'})
                 }
             });
-
             $gnbWrap.on({
                 mouseenter:function(){
                     $nav.addClass('mouseon');
                     $logo.css({color:'#000'});
+                    $main.css({filter:'brightness(30%)'})
                 }
             });
             
@@ -71,6 +68,21 @@
                 }
             });
 
+            $langKR.on({
+                click:function(){
+                    $langEN.toggle();
+                }
+            });
+
+            $sideBtn.on({
+                mouseenter:function(){
+                    $sideBtnText.stop().slideDown(200);
+                },
+                mouseleave:function(){
+                    $sideBtnText.stop().slideUp(200);
+                }
+            });
+
         },
 
         section1Fn:function(){
@@ -79,12 +91,16 @@
             var $winH = $(window).height;
             var $mainBgSlide = $('#section1 .mainBg-slide');
             var $mainBgSlideWrap = $('#section1 .mainBg-slide-wrap');
-            var $mainBgSlide = $('#section1 .mainBg-slide');
+            var $mainBgSlideView = $('#section1 .mainBg-slide-view');
             var $prevBtn = $('#section1 .prev-btn');
             var $nextBtn = $('#section1 .next-btn');
+            var $startBtn = $('#section1 .start-btn');
             var $stopBtn = $('#section1 .stop-btn');
-            var $mainBgSlideView = $('#section1 .mainBg-slide-view');
+            var $mainBgBtnWrap = $('#section1 .mainBg-Btn-wrap');
             var slideCnt = 0;
+            var setId = null;
+            var setId2 = null;
+            var timercnt = 0;
             
 
             function resizeFn(){
@@ -103,7 +119,6 @@
                 $mainBgSlideWrap.stop().animate({left:-$winW*slideCnt}, 500, 'easeOutCubic',function(){
                     if ( slideCnt > 2 ) { slideCnt = 0; }
                     if ( slideCnt < 0 ) { slideCnt = 2; }
-                    console.log(slideCnt);
                     $mainBgSlideWrap.stop().animate({left:-$winW*slideCnt}, 0)
                 })
             }
@@ -119,6 +134,7 @@
 
             $prevBtn.on({
                 click:function(){
+                    bgSlideTimerFn();
                     if (!$mainBgSlideWrap.is(':animated')) {
                         bgPrevCountFn();
                     }
@@ -126,6 +142,7 @@
             });
             $nextBtn.on({
                 click:function(){
+                    bgSlideTimerFn();
                     if (!$mainBgSlideWrap.is(':animated')) {
                         bgNextCountFn();
                     }
@@ -134,20 +151,112 @@
 
             $mainBgSlideView.swipe({
                 swipeLeft:function(){
+                    bgSlideTimerFn();
                     if (!$mainBgSlideWrap.is(':animated')) {
                         bgNextCountFn();
                     }
                 },
                 swipeRight:function(){
+                    bgSlideTimerFn();
                     if (!$mainBgSlideWrap.is(':animated')) {
                         bgPrevCountFn();
                     }
                 }
             });
+
+            function bgSlideAutoPlayFn(){
+                setId = setInterval(bgNextCountFn, 4000);
+            }
+            bgSlideAutoPlayFn();
+
+            function bgSlideTimerFn(){
+                timercnt = 0;
+                clearInterval(setId);
+                clearInterval(setId2);
+                setId2 = setInterval(function(){
+                    timercnt++;
+                    if (timercnt >= 4) {
+                        clearInterval(setId2);
+                        bgNextCountFn();
+                        bgSlideAutoPlayFn();
+                    }
+                }, 1000);
+            }
+            // stop 버튼클릭시 stopBtn에 클래스 부여 display:none;
+            // stop 버튼클릭시 startBtn에 클래스 부여 display:inline-block;
+            // start 버튼클릭시 startBtn에 클래스 제거 display:none;
+            // start 버튼클릭시 stopBtn에 클래스 제거 display:inline-block;
+            $stopBtn.on({
+                click:function(){
+                    $stopBtn.addClass('mouseon');
+                    $startBtn.addClass('mouseon');
+                }
+            });
+            $startBtn.on({
+                click:function(){
+                    $startBtn.removeClass('mouseon');
+                    $stopBtn.removeClass('mouseon');
+                }
+            });
             
+
         },
 
         section2Fn:function(){
+            var $section2SlideView = $('#section2 .slide-view');
+            var $section2SlideWrap = $('#section2 .slide-wrap');
+            var $section2Slide = $('#section2 .slide');
+            var $s2SlideCnt = 0;
+
+            var $roomWhiteBtn = $('#section2 .room-whitebtn');
+            var $roomBlackBtn = $('#section2 .room-blackbtn');
+
+            function section2SlideFn(){
+                $section2SlideWrap.stop().animate({left:-1050*$s2SlideCnt}, 300, 'easeOutCubic', function(){
+                    if ( $s2SlideCnt > 4 ) { $s2SlideCnt = 0; }
+                    if ( $s2SlideCnt < 0 ) { $s2SlideCnt = 4; }
+                    $section2SlideWrap.stop().animate({left:-1050*$s2SlideCnt}, 0)
+                })
+            }
+
+            function section2PrevCountFn(){
+                $s2SlideCnt--;
+                section2SlideFn();
+            }
+
+            function section2NextCountFn(){
+                $s2SlideCnt++;
+                section2SlideFn();
+            }
+
+            $section2SlideView.swipe({
+                swipeLeft:function(){
+                    if (!$section2SlideWrap.is(':animated')) {
+                        section2NextCountFn();
+                    }
+                },
+                swipeRight:function(){
+                    if (!$section2SlideWrap.is(':animated')) {
+                        section2PrevCountFn();
+                    }
+                }
+            });            
+
+            function section2SlideAutoPlayFn(){
+                setInterval(section2NextCountFn, 4000)
+            }
+            section2SlideAutoPlayFn();
+
+            $roomWhiteBtn.on({
+                mouseenter:function(){
+                    $roomBlackBtn.addClass('mouseon');
+                }
+            });
+            $roomBlackBtn.on({
+                mouseleave:function(){
+                    $roomBlackBtn.removeClass('mouseon');
+                }
+            });
 
         },
 
