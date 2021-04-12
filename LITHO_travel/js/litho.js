ce2f2cb730ca15742f1ac4bd1351a7caff6990c4
serene1004 <vcbxnzn@naver.com>
2021-04-12 18:36:28 +0900

    update 2021-04-12

--- LITHO_travel/js/litho.js
+++ LITHO_travel/js/litho.js
@@ -17,27 +17,109 @@
             that.footerFn();
         },
         headerFn:function(){
+            var $nav     = $('#header #nav');
+            var $mainBtn = $('#header .main-btn');
+            var $sub     = $('#header .sub');
+            var $subBtn    = $('#header .sub-btn');
+            var $subSub     = $('#header .sub-sub');
+            var $subSubBtn    = $('#header .sub-sub-btn');
+            var $subSubSub     = $('#header .sub-sub-sub');
 
+            
+            $mainBtn.on({
+                mouseenter:function(){
+                    $sub.hide();
+                    $(this).next().show();
+                }
+            });
+            $subBtn.on({
+                mouseenter:function(){
+                    $subSub.hide();
+                    $(this).next().show();
+                }
+            });
+            $subSubBtn.on({
+                mouseenter:function(){
+                    $subSubSub.hide();
+                    $(this).next().show();
+                }
+            })
+
+            $nav.on({
+                mouseleave:function(){
+                    $sub.hide();
+                    $subSub.hide();
+                }
+            });
+            
         },
         section1Fn:function(){
+            var $winW      = $(window).width();
+            var $winH      = $(window).height();
+            var $section1  = $('#section1');
             var $slideView = $('#section1 .slide-view');
             var $slideWrap = $('#section1 .slide-wrap');
-            var $slide = $('#section1 .slide');
-            var slideW = $('#section1 .slide').innerWidth();
-            var cnt = 0;
-            var touchS = 0;
-            var touchE = 0;
+            var $slide     = $('#section1 .slide');
+            var slideW     = $('#section1 .slide').innerWidth();
+            var n          = $('#section1 .slide').length;  // n=3
+            var cnt        = 0;
+            var touchS     = 0;
+            var touchE     = 0;
+            var touchD     = false;
+            var next       = [];
+            var prev       = [];
+
+            function resizeFn(){
+                $winW = $(window).width();
+                $winH = $(window).height();
+                $slide.css({width:$winW,height:$winH});
+                $section1.css({width:$winW,height:$winH});
+            }
+
+            $(window).resize(function(){
+                resizeFn();
+            });
+            setTimeout(resizeFn, 100);
 
             function mainPrevSlideFn(){
-                $slide.css({zIndex:1}).stop().animate({opacity:1},0);
-                $slide.eq(cnt).css({zIndex:2});
-                $slide.eq(cnt===2?0:cnt+1).css({zIndex:3}).stop().animate({opacity:1},0).animate({opacity:0},1000);
+                var j = n;
+                for(var i=0; i<n; i++){
+                    j--;
+                    prev[i] = j;
+                }
+
+                var temp = prev.pop();
+                           prev.unshift(temp);
+
+                for(var i=n-1; i>cnt; i--){
+                    var temp = prev.shift();
+                               prev.push(temp);
+                }
+
+                for(var i=0; i<n; i++){
+                    $slide.eq(prev[i]).stop().animate({left:(100*(-i))+'%'}, 0).animate({left:(100*(-i+1))+'%'}, 600, 'easeInOutCubic');
+                }
             }
             function mainNextSlideFn(){
-                $slide.css({zIndex:1});
-                $slide.eq(cnt===0?2:cnt-1).css({zIndex:2});
-                $slide.eq(cnt).css({zIndex:3}).stop().animate({opacity:0},0).animate({opacity:1},1000);
+                for(var i=0; i<n; i++){
+                    next[i] = i;
+                    // console.log('넥스트 설정',next);
+                }
+                var temp = next.pop();
+                           next.unshift(temp);
+                        //    console.log('임시부여이후',next);
+
+                for(var i=0; i<cnt; i++){
+                    var temp = next.shift();
+                               next.push(temp);
+                            //    console.log('푸쉬이후',next);
+                 }
+
+                for(var i=0; i<n; i++){
+                    $slide.eq(next[i]).stop().animate({left:(100*i)+'%'}, 0).animate({left:(100*(i-1))+'%'}, 600, 'easeInOutCubic');
+                }
             }
+
             function prevSlideCountFn(){
                 cnt--;
                 if(cnt<0){cnt=2;}
@@ -51,15 +133,25 @@
 
             $slideView.on({
                 mousedown:function(e){
+                    touchD = true;
                     e.preventDefault();
                     touchS = e.clientX;
                 },
                 mouseup:function(e){
+                    touchD = false;
                     e.preventDefault();
                     touchE = e.clientX;
                     touchSwipeFn();
+                },
+                mouseleave:function(e){
+                    if(touchD === true){
+                        touchE = e.clientX;
+                        touchSwipeFn();
+                        touchD = false;
+                    }
                 }
             });
+
             function touchSwipeFn(){
                 if(touchS-touchE > 0){
                     if(!$slide.is(':animated')){
@@ -72,7 +164,6 @@
                     }
                 }
             }
-
 
         },
         section2Fn:function(){
